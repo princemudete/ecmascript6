da51d267144995e0683e0f8959a1e2dcac292681
princemudete <princemudete@gmail.com>
2025-08-01 10:05:13 +0200

    Updated layout for wrapper and nav

--- javascript.js
+++ javascript.js
@@ -1,25 +1,7 @@
 window.onload = function() {
-    function log(num=10) {
-        console.log(num);
-    }
-    log(5);
-
-    function logNinja(name, belt, age) {
-        console.log("My name is " + name +
-                    " and my belt colour is " + belt +
-                    " and my age is " + age);
-    }
-    logNinja("Shaun", "Orange", 66);
-
-    var nums1 =[ 1, 2, 3];
-    var nums2 = [nums1, 4, 5, 6];
-
-    console.log(nums2);
-
-    var nums = [3, 5, 7];
-
-    function addNums(a,b,c) {
-        console.log(a + b + c);
-    }
-    addNums(...nums);
+    
+    function logNinja(name, age) {
+        console.log("my name is ${name} and my ${age}"); 
+    } 
+    logNinja("John", 25);
 }    
